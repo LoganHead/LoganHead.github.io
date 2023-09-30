a0083c07947aa85eef0dc211128ae51d071ae00f
LoganHead <100407576+LoganHead@users.noreply.github.com>
2023-09-30 21:47:47 +1300

    back to normal

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-// Check if the user is logged in (this is a simplified example)
-const isLoggedIn = true; // Change this based on your authentication logic
-
-// Function to load content based on user's login status
-function loadContent() {
-    const contentDiv = document.getElementById('content');
-
-    if (isLoggedIn) {
-        contentDiv.innerHTML = '<h2>Welcome, User!</h2><p>This is your personalized content.</p>';
-    } else {
-        contentDiv.innerHTML = '<h2>Welcome to our website!</h2><p>Please log in to access exclusive content.</p>';
-    }
-}
-
-// Call the function when the page loads
-window.onload = loadContent;
+console.log('hello world');
